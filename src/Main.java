b0d6e9c8d0802e674028d530e0595c0b0e87d8bf
22373400 <22373400@buaa.edu.cn>
2025-10-30 17:59:51 +0800

    将需要检测的图片像素输入到output.txt文件中，但是图片过大，处理需要非常长的时间，建议用一部分图片做测试。

--- src/Main.java
+++ src/Main.java
@@ -9,15 +9,31 @@ import org.opencv.highgui.HighGui;
 import org.opencv.imgcodecs.Imgcodecs;
 
 import java.awt.*;
-import java.io.FileNotFoundException;
-import java.io.PrintWriter;
+import java.io.*;
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
         System.load("C:\\Users\\Leon\\Desktop\\opencv\\build\\java\\x64\\opencv_java480.dll");
 
-        Mat mat = new Mat(200, 200, CvType.CV_8UC3, new Scalar(255, 0, 0));
-        HighGui.imshow("mat", mat);
-        HighGui.waitKey();
+        Mat mat = Imgcodecs.imread("C:\\Users\\Leon\\Desktop\\nVisual\\test.png");
+        System.out.println(mat);
+        try {
+            FileOutputStream fileOutputStream = new FileOutputStream("output.txt");
+            BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(fileOutputStream));
+            for (int i = 0; i < mat.rows(); i ++){
+                for (int j = 0; j < mat.cols(); j ++){
+                    String string = Arrays.toString(mat.get(i,j));
+                    bufferedWriter.write(string);
+                    System.out.println(string);
+                }
+            }
+            bufferedWriter.close();
+        }catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+
+
     }
 }
