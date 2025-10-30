44d3af3746f6deb5928b71f435f3083db2a2d026
22373400 <22373400@buaa.edu.cn>
2025-10-30 15:38:58 +0800

    项目检测图片

--- src/Main.java
+++ src/Main.java
@@ -4,9 +4,22 @@
 "C:\Users\Leon\Desktop\opencv\build\java\x64\opencv_java480.dll"
  */
 
+import org.opencv.core.Core;
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
+import org.opencv.core.Size;
+import org.opencv.imgcodecs.Imgcodecs;
+
+import java.awt.*;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+
 public class Main {
     public static void main(String[] args) {
+        System.load("C:\\Users\\Leon\\Desktop\\opencv\\build\\java\\x64\\opencv_java480.dll");
 
-
+        Mat image = Imgcodecs.imread("C:\\Users\\Leon\\Desktop\\nVisual\\test.png");
+        System.out.println(image);
+        System.out.println(image.dump());
     }
 }
