8f79a6b965eab7e05df208533e67130caa0360d4
22373400 <22373400@buaa.edu.cn>
2025-10-30 14:51:37 +0800

    项目结构新增opencv 4.8.0

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,8 @@
+/*
+                        环境依赖
+"C:\Users\Leon\Desktop\opencv\build\java\opencv-480.jar"
+"C:\Users\Leon\Desktop\opencv\build\java\x64\opencv_java480.dll"
+ */
 
 public class Main {
     public static void main(String[] args) {
