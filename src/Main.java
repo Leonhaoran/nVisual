31d5ef4e8c391960c8b985797130ad5fe94aae2c
22373400 <22373400@buaa.edu.cn>
2025-11-03 14:11:29 +0800

    学习到Rect

--- src/Main.java
+++ src/Main.java
@@ -5,35 +5,27 @@
  */
 
 import org.opencv.core.*;
+import org.opencv.core.Point;
 import org.opencv.highgui.HighGui;
 import org.opencv.imgcodecs.Imgcodecs;
+import org.opencv.imgproc.Imgproc;
 
-import java.awt.*;
+
 import java.io.*;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
         System.load("C:\\Users\\Leon\\Desktop\\opencv\\build\\java\\x64\\opencv_java480.dll");
 
-        Mat mat = Imgcodecs.imread("C:\\Users\\Leon\\Desktop\\nVisual\\test.png");
-        System.out.println(mat);
-        try {
-            FileOutputStream fileOutputStream = new FileOutputStream("output.txt");
-            BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(fileOutputStream));
-            for (int i = 0; i < mat.rows(); i ++){
-                for (int j = 0; j < mat.cols(); j ++){
-                    String string = Arrays.toString(mat.get(i,j));
-                    bufferedWriter.write(string);
-                    System.out.println(string);
-                }
-            }
-            bufferedWriter.close();
-        }catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-
-
-
+        double[] vals = {1, 2, 3, 4, 5, 6, 7, 8};
+        Rect rect = new Rect(2, 5, 3, 4);
+        System.out.println("rect = " + rect);
+        System.out.println("rect.tl() = " + rect.tl());
+        System.out.println("rect.br() = " + rect.br());
+        System.out.println("rect.contains(new Point(5,9)) = " + rect.contains(new Point(4.9, 8.9)));
     }
 }
