832db80868c396a226a862b6c930a4f5202a678d
22373400 <22373400@buaa.edu.cn>
2025-11-04 14:47:44 +0800

    使用Canny算法检测图像的边缘，目前仍存在将文本内容检测为矩形、不规则图形检测为多个小矩形等问题

--- src/Main.java
+++ src/Main.java
@@ -1,51 +1,60 @@
-/*
-                        环境依赖
-"C:\Users\Leon\Desktop\opencv\build\java\opencv-480.jar"
-"C:\Users\Leon\Desktop\opencv\build\java\x64\opencv_java480.dll"
- */
-
 import org.opencv.core.*;
-import org.opencv.core.Point;
 import org.opencv.highgui.HighGui;
 import org.opencv.imgcodecs.Imgcodecs;
 import org.opencv.imgproc.Imgproc;
 
-
-import java.io.*;
-import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.List;
-import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
         System.load("C:\\Users\\Leon\\Desktop\\opencv\\build\\java\\x64\\opencv_java480.dll");
 
-//        Mat mat1 = new Mat(3, 3, CvType.CV_8UC1);
-//        mat1.put(0, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9);
-//        Mat mat2 = new Mat(3, 3, CvType.CV_8UC1);
-//        mat2.put(0, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9);
-//        Mat dst = new Mat();
-//        Core.add(mat1, mat2, dst);
-//        System.out.println("dst.dump() = \n" + dst.dump());
+        Mat image = Imgcodecs.imread("C:\\Users\\Leon\\Desktop\\nVisual\\test_micro.png");
 
+        // 转换为灰度图
+        Mat gray = new Mat();
+        Imgproc.cvtColor(image, gray, Imgproc.COLOR_BGR2GRAY);
 
-//        Mat diff = new Mat();
-//        Core.absdiff(mat2, mat1, diff);
-//        System.out.println("diff.dump() = \n" + diff.dump());
+//        HighGui.imshow("gray", gray);
 
-        Mat imread1 = Imgcodecs.imread("C:\\Users\\Leon\\Desktop\\dog.png");
-        Mat imread2 = Imgcodecs.imread("C:\\Users\\Leon\\Desktop\\jail.jpg");
+        // 图像反向二值化，去除噪点
+        Mat threshold = new Mat();
+        Imgproc.threshold(gray, threshold, 0, 255, Imgproc.THRESH_BINARY_INV | Imgproc.THRESH_OTSU);
+//        HighGui.imshow("threshold", threshold);
+
+        // 像素点加粗，让矩形闭合
+        Mat element = Imgproc.getStructuringElement(Imgproc.MORPH_RECT, new Size(3, 3));
         Mat dst = new Mat();
-        Imgproc.resize(imread1, imread1, imread2.size());
-        System.out.println("imread1 = " + imread1);
-        System.out.println("imread2 = " + imread2);
-        Core.addWeighted(imread1, 0.5, imread2, 0.5, -50, dst);
-        Imgcodecs.imwrite("result.png", dst);
-        HighGui.imshow("dst", dst);
+        Imgproc.morphologyEx(threshold, dst, Imgproc.MORPH_GRADIENT, element);
+//        HighGui.imshow("dst", dst);
 
+        // 使用Canny算法检测图像的边缘
+        Mat edges = new Mat();
+        Imgproc.Canny(dst, edges, 50, 150);
 
+        // 找轮廓
+        List<MatOfPoint> contours = new LinkedList<>();
+        Mat hierarchy = new Mat();
+        Imgproc.findContours(edges, contours, hierarchy, Imgproc.RETR_EXTERNAL, Imgproc.CHAIN_APPROX_SIMPLE);
+
+        int total = 0;
+        for (MatOfPoint contour : contours) {
+            Rect boundRect = Imgproc.boundingRect(contour);
+//            System.out.println(Math.abs((double) boundRect.width / boundRect.height - 2.0));
+            if (Math.abs((double) boundRect.width / boundRect.height - 2.0) < 0.05){
+                total++;
+                System.out.println(total);
+                System.out.println("boundRect.tl() = " + boundRect.tl());
+                System.out.println("boundRect.br() = " + boundRect.br());
+                System.out.println();
+            }
+
+            Imgproc.rectangle(image, boundRect.tl(), boundRect.br(), new Scalar(0, 0, 255), 2);
+        }
+
+        Imgcodecs.imwrite("result.png", image);
+        HighGui.imshow("result", image);
         HighGui.waitKey();
-
     }
 }
