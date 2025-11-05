50e6aa931ec4c635a3336c93578a86fdb32e6247
22373400 <22373400@buaa.edu.cn>
2025-11-05 10:23:48 +0800

    可以较好地检测小图片中指定长和宽（单位：像素）的矩形，但是对于大图片无法检测，怀疑是爆内存

--- src/Main.java
+++ src/Main.java
@@ -3,20 +3,16 @@ import org.opencv.highgui.HighGui;
 import org.opencv.imgcodecs.Imgcodecs;
 import org.opencv.imgproc.Imgproc;
 
-import javax.swing.*;
-import javax.swing.event.ChangeEvent;
-import javax.swing.event.ChangeListener;
-import java.awt.*;
-import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.Map;
 
+// 机柜：600*1200 600*1100 600*1000 800*1200 800*1100 800*1000
+// width / height = 1.83
 public class Main {
     public static void main(String[] args) {
         System.load("C:\\Users\\Leon\\Desktop\\opencv\\build\\java\\x64\\opencv_java480.dll");
 
-        Mat image = Imgcodecs.imread("C:\\Users\\Leon\\Desktop\\nVisual\\test_micro.png");
+        Mat image = Imgcodecs.imread("C:\\Users\\Leon\\Desktop\\nVisual\\test_small_2_2.png");
 
         // 转换为灰度图
         Mat gray = new Mat();
@@ -37,29 +33,54 @@ public class Main {
 
         // 使用Canny算法检测图像的边缘
         Mat edges = new Mat();
-        Imgproc.Canny(dst, edges, 50, 150);
+        Imgproc.Canny(dst, edges, 50, 100);
 
         // 找轮廓
         List<MatOfPoint> contours = new LinkedList<>();
         Mat hierarchy = new Mat();
         Imgproc.findContours(edges, contours, hierarchy, Imgproc.RETR_EXTERNAL, Imgproc.CHAIN_APPROX_SIMPLE);
 
+        System.out.println(contours.size());
         int total = 0;
         for (MatOfPoint contour : contours) {
             Rect boundRect = Imgproc.boundingRect(contour);
             double width = boundRect.width;
             double height = boundRect.height;
-            if (Math.abs(width / height - 2.0) < 0.5 && Math.abs(width - 68) < 3 && Math.abs(height - 37) < 3) {
+            if (Math.abs(width / height - 1.83) < 0.5 && Math.abs(width - 68) < 3 && Math.abs(height - 37) < 3) {
                 total++;
                 System.out.println(total);
-                System.out.println("Math.abs((double) boundRect.width / boundRect.height - 2.0) = " + Math.abs(width / height - 2.0));
+                System.out.println("Red");
                 System.out.println("width = " + width);
                 System.out.println("height = " + height);
                 System.out.println("boundRect.tl() = " + boundRect.tl());
                 System.out.println("boundRect.br() = " + boundRect.br());
                 System.out.println();
                 Imgproc.rectangle(image, boundRect.tl(), boundRect.br(), new Scalar(0, 0, 255), 2);
+            } else if (Math.abs(width / height - 1.36) < 0.5 && Math.abs(width - 67) < 3 && Math.abs(height - 49) < 3) {
+                total++;
+                System.out.println(total);
+                System.out.println("Blue");
+                System.out.println("width = " + width);
+                System.out.println("height = " + height);
+                System.out.println("boundRect.tl() = " + boundRect.tl());
+                System.out.println("boundRect.br() = " + boundRect.br());
+                System.out.println();
+                Imgproc.rectangle(image, boundRect.tl(), boundRect.br(), new Scalar(255, 0, 0), 2);
             }
+//            else if (Math.abs(width / height - 1.83) < 1 && Math.abs(width - 69) < 10 && Math.abs(height - 38) < 10) {
+//                total++;
+//                System.out.println(total);
+//                System.out.println("Green");
+//                System.out.println("width = " + width);
+//                System.out.println("height = " + height);
+//                System.out.println("boundRect.tl() = " + boundRect.tl());
+//                System.out.println("boundRect.br() = " + boundRect.br());
+//                System.out.println();
+//                Imgproc.rectangle(image, boundRect.tl(), boundRect.br(), new Scalar(0, 255, 0), 2);
+//            }
+
+
+//            Imgproc.rectangle(image, boundRect.tl(), boundRect.br(), new Scalar(0, 255, 0), 2);
 
 
         }
